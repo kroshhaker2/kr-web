ac72f2ab1040198d8093acec20f1d3f5f408a398
krosh <krosh@kroshhaker.dev>
2026-08-30 17:11:31 +0300

    feat: set up routing with admin layout

---
+++ src/layouts/AdminLayout.tsx
@@ -0,0 +1,6 @@
+//import { Outlet } from "@solidjs/router";
+//import Sidebar from "@/components/admin/Sidebar";
+
+export default function AdminLayout() {
+    return <div class="admin-layout"></div>;
+}
